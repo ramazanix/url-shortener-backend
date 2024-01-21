45d6f03675828f9d4b61fae41127364d9b6bc4fd
ramazanix <ramazanui2002@gmail.com>
2024-01-21 15:51:26 +0500

    Created test schemas for urls

---
+++ tests/urls/schemas.py
@@ -0,0 +1,11 @@
+url_base = {"full_name": str, "short_name": str}
+url = {
+    "id": str,
+    "full_name": str,
+    "short_name": str,
+    "created_at": str,
+    "user_id": str,
+}
+
+
+urls_base: list[url_base] = [url_base]
