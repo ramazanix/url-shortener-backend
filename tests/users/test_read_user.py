4826ad19d7766d776de7dccf4e53171f4129d445
ramazanix <ramazanui2002@gmail.com>
2024-01-21 15:49:28 +0500

    Created tests for reading user

--- tests/users/test_read_user.py
+++ tests/users/test_read_user.py
@@ -1,8 +1,14 @@
 import pytest
 from httpx import AsyncClient
 from pytest_schema import exact_schema
-from ..conftest import create_user, authorization_header
+from ..conftest import (
+    create_user,
+    create_default_url,
+    create_custom_url,
+    authorization_header,
+)
 from .schemas import user
+from ..urls.schemas import urls_base
 
 
 @pytest.mark.asyncio
@@ -25,3 +31,48 @@ async def test_read_current_user_unauthorized(client: AsyncClient):
     """
     response = await client.get("/users/me")
     assert response.status_code == 401
+
+
+@pytest.mark.asyncio
+async def test_read_current_user_blank_urls_authorized(
+    client: AsyncClient, create_user, authorization_header
+):
+    """
+    Testing users without url path authorized
+    """
+    response = await client.get("/users/me/urls", headers=authorization_header)
+    assert response.status_code == 200
+    assert response.json() == []
+
+
+@pytest.mark.asyncio
+async def test_read_current_user_blank_urls_unauthorized(
+    client: AsyncClient, create_user
+):
+    """
+    Testing users without url path unauthorized
+    """
+    response = await client.get("/users/me/urls")
+    assert response.status_code == 401
+
+
+@pytest.mark.asyncio
+async def test_read_current_user_urls_unauthorized(client: AsyncClient, create_user):
+    """
+    Testing users with url path unauthorized
+    """
+    response = await client.get("/users/me/urls")
+    assert response.status_code == 401
+
+
+@pytest.mark.asyncio
+async def test_read_current_user_urls_authorized(
+    client: AsyncClient, create_user, create_default_url, authorization_header
+):
+    """
+    Testing users with url path authorized
+    """
+    response = await client.get("/users/me/urls", headers=authorization_header)
+    assert response.status_code == 200
+    assert response.json() != []
+    assert exact_schema(urls_base) == response.json()
